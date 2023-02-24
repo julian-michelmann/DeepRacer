3a7e7d8be965df0a1e16863f723e71e3c4a3e486
Julian Akkaya <julian.akkaya@axelspringer.com>
2023-02-24 16:58:22 +0100

    Trying new approche for the reward_distance_from_center function

--- DeepRacer/reward.py
+++ DeepRacer/reward.py
@@ -29,22 +29,14 @@ def reward_function(params):
 
 
 def reward_distance_from_center(track_width, all_wheels_on_track, is_reversed, distance_from_center):
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
-
-    # Give higher reward if the car is closer to center line and vice versa
     if not all_wheels_on_track:
         reward = 1e-2
     elif is_reversed:
         reward = 1e-2
-    elif distance_from_center <= marker_1:
-        reward = 1
-    elif distance_from_center <= marker_2:
-        reward = 0.5
-    elif distance_from_center <= marker_3:
-        reward = 0.1
     else:
+        reward = - 1 / track_width * distance_from_center + 1
+
+    if reward <= 0:
         reward = 1e-2
 
     return reward
--- Plot/distance_from_center.py
+++ Plot/distance_from_center.py
@@ -4,18 +4,22 @@ from matplotlib import pyplot as plt
 from DeepRacer.reward import reward_distance_from_center
 
 
+TRACK_WIDTH_NEW = 5
+
+
 def plot_distance_from_center_reward():
     x_points = []
     y_points = []
 
-    for i in np.arange(0, 10, 0.1):
+    for i in np.arange(0, TRACK_WIDTH_NEW + 3, 0.1):
         x_points.append(i)
 
     for x_point in x_points:
-        reward = reward_distance_from_center(10, True, False, x_point)
+        reward = reward_distance_from_center(TRACK_WIDTH_NEW, True, False, x_point)
         y_points.append(reward)
 
     plt.plot(np.array(x_points), np.array(y_points))
+
     plt.title("Reward Distance to Center")
     plt.xlabel("Distance to Center")
     plt.ylabel("Reward")
